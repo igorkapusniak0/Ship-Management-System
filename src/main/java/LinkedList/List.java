126b10d0305596c37341c6268770e4ae66abbc0d
ikapu <20102236@mail.wit.ie>
2023-10-10 20:13:01 +0100

    Initial commit

---
+++ src/main/java/LinkedList/List.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+public class List<T> {
+    private Node<T> head;
+    public List(){
+        this.head = null;
+    }
+    public void add(T data){
+        Node<T> newNode = new Node<>(data);
+
+        if (head == null){
+            head = newNode;
+        }
+        else {
+            Node<T> current = head;
+            while (current.next != null){
+                current=current.next;
+            }
+            current.next = newNode;
+        }
+    }
+}
